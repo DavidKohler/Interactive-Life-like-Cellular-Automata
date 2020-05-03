efbd555bd67c336ffd8eed4f7b15297e4c255636
David Kohler <dako.davidkohler@gmail.com>
2020-05-03 01:54:54 -0500

    Updates

---
+++ precache-manifest.60ebeedc4718428e02e1f3c681324b59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbc1911524653f6e1b4441f2ce181982",
+    "url": "/Game-of-Life-Interactive/index.html"
+  },
+  {
+    "revision": "34a9d6ad6dd94e22228f",
+    "url": "/Game-of-Life-Interactive/static/css/2.9974c224.chunk.css"
+  },
+  {
+    "revision": "66c4efa453006dae7a2f",
+    "url": "/Game-of-Life-Interactive/static/css/main.2a92997c.chunk.css"
+  },
+  {
+    "revision": "34a9d6ad6dd94e22228f",
+    "url": "/Game-of-Life-Interactive/static/js/2.d0d62104.chunk.js"
+  },
+  {
+    "revision": "86bf7d79e2d95c812ddd8e211e299cab",
+    "url": "/Game-of-Life-Interactive/static/js/2.d0d62104.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66c4efa453006dae7a2f",
+    "url": "/Game-of-Life-Interactive/static/js/main.b809bb39.chunk.js"
+  },
+  {
+    "revision": "87eae140f45c7ad060a7",
+    "url": "/Game-of-Life-Interactive/static/js/runtime-main.35984725.js"
+  }
+]);
